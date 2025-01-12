9318af4ec60f369cd1ff9c09693857575ce8524b
Ioni <ionipatroescu203@gmail.com>
2025-01-12 02:51:19 +0200

    updated credentials

--- Big.Data.DataCollection/Models/DbEntities/SocialMediaComment.cs
+++ Big.Data.DataCollection/Models/DbEntities/SocialMediaComment.cs
@@ -1,12 +1,20 @@
-﻿namespace Big.Data.DataCollection.Models.DbEntities;
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Big.Data.DataCollection.Models.DbEntities;
 
 public class SocialMediaComment
 {
+    [BsonId]
     public required string Id { get; set; }
 
+    [BsonElement("name")]
     public required string Name { get; set; }
 
+    [BsonElement("date")]
     public required string Date { get; set; }
 
+    [BsonElement("comment")]
     public required string Comment { get; set; }
+
+    public SocialMediaComment() { }
 }
